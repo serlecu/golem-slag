ac4b9d91a1982b709d228c8a7ebcd81bbe966190
Sergio Lecuona <serlecu@hotmail.com>
2023-07-19 12:50:53 +0200

    clean and rename autorun sript

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,11 @@
 #! usr/bin/bash
-cd ~/Desktop/golem-node-screen
+# navigate inside the repository
+cd ~/Desktop/golem-slag
+# try update repository
 git pull || echo "no connection" 
 
 echo "Check requirements"
-pip install -r node_display/requirements.txt
+pip install -r requirements.txt
 
 echo "Config Bluetooth"
 # some lines may not be required on each reboot
@@ -14,25 +16,10 @@ sudo sdptool add SP
 sudo chmod o+rw /var/run/sdp
 sudo hciconfig hci0 piscan
 
-echo "Config Rail i2C for Hyperp-Pixel"
-sudo ln -s /dev/i2c-11 /dev/i2c-1
-
-# Generate the .log file name
-index=0
-filename="logfile.log"
-# Check if the file already exists
-while [[ -e "$filename" ]]; do
-  index=$((index + 1))
-  filename="logfile_$index.log"
-done
-
 echo "run python"
-python ~/Desktop/golem-node-screen/node_display/node-display.py >> "$filename" 2>&1
+python ~/Desktop/golem-slag/main.py >> "$filename" 2>&1
 
-# echo "run processing"
-#~/Desktop/golem-node-screen/node_display/application.linux-arm64/node_display &
-
-# == Do perform only once == wait...
+# == Do perform manualy only once ==
 # add user to bluetooth group
 # ~ sudo usermod -a -G bluetooth $USER
 # In 'sudo nano /etc/systemd/system/bluetooth.target.wants/bluetooth.service' modify or add 'ExecStart=-/usr/lib/bluetooth/bluetoothd -- experimental'  
