13f4fc265dd9d92802783b2693f21774da67cfbc
Sergio Lecuona <serlecu@hotmail.com>
2023-07-31 16:58:48 +0200

    Update serial_send.py

--- src/serial_send.py
+++ src/serial_send.py
@@ -61,30 +61,30 @@ def railSerialThread():
           g.serialState = True
 
           while g.serialState:
-              if checkConnected(port):
+              # if checkConnected(port):
                   if not g.offlineMode:
                       try:
                           sendValueSerial(arduino, g.railDelay ) #send millis
                       except Exception as e:
                           print(f"Serial ERROR: {e}")
-                          # g.serialState = False
-                          # try:
-                          #     #arduino.__del__()
-                          #     arduino.close()
-                          # except Exception as e:
-                          #     print(f"Serial ERROR: {e}")
+                          g.serialState = False
+                          try:
+                              #arduino.__del__()
+                              arduino.close()
+                          except Exception as e:
+                              print(f"Serial ERROR: {e}")
                       finally:
                           time.sleep(2)
                   else:
                       randomVal = random.randint(500, 1000)
                       sendValueSerial(arduino, randomVal)
-              else:
-                  g.serialState = False
-                  try:
-                      #arduino.__del__()
-                      arduino.close()
-                  except Exception as e:
-                      print(f"Serial ERROR: {e}")
+              # else:
+              #     g.serialState = False
+              #     try:
+              #         #arduino.__del__()
+              #         arduino.close()
+              #     except Exception as e:
+              #         print(f"Serial ERROR: {e}")
         finally:
             time.sleep(2)
         print("Lost Arduino connection")
